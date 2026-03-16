c450af35b55b8ad5908b73042c9db55445efc37a
Akash Vijayan <akash.it2027@gmail.com>
2026-03-16 15:31:37 +0530

    Day-2

---
+++ Day -2/for Loop.py
@@ -0,0 +1,16 @@
+fruits =['Apple','Grapes','blueberry']
+for x in fruits :
+    print(x)
+
+
+city = ('frankfurt','berlin','jerusalem','london')
+for Y in city :
+    if Y == 'berlin' :
+        continue
+    print(Y)
+
+city = ['frankfurt','berlin','jerusalem','london']
+for Q in city :
+    if Q == 'berlin' :
+        break
+    print(Q)
