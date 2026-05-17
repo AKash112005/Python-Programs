ccdfa2b7598ecdad3570c13f42b8f87423f469b8
Akash <akash.it2027@gmail.com>
2026-05-17 21:51:09 +0530

    Refactor for loop structure in for Loop.py

--- Day -2/for Loop.py
+++ Day -2/for Loop.py
@@ -1,13 +1,11 @@
 fruits =['Apple','Grapes','blueberry']
 for x in fruits :
     print(x)
-
 city = ('frankfurt','berlin','jerusalem','london')
 for Y in city :
     if Y == 'berlin' :
         continue
     print(Y)
-
 city = ['frankfurt','berlin','jerusalem','london']
 for Q in city :
     if Q == 'berlin ' :
